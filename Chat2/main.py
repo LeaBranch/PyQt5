aeeac8b36837bf1ef01ac49a2bbdf2c4f3bcaaff
elizaveta <rubanskaya.elizaveta@mail.ru>
2020-09-30 16:38:27 +0700

    add output thread

--- Chat2/main.py
+++ Chat2/main.py
@@ -3,23 +3,21 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QApplication, QDesktopWidget
 
 from gui import Ui_MainWindow  # импорт нашего сгенерированного файла
-import sys
+import sys, os
  
+
 name = ''
-with open("log.txt") as f:
-    text = f.read()
 
 class Output(QtCore.QThread):
-    global text
-    # text = f.read()
-    mySignal1 = QtCore.pyqtSignal()
-    mySignal2 = QtCore.pyqtSignal(str)
+    global f
+    mySignal1 = QtCore.pyqtSignal(str)
+
     def __init__(self, parent=None):
         QtCore.QThread.__init__(self, parent)
-        def run(self):
-                self.mySignal1.emit()
-                self.mySignal2.emit(text)
-                print(text)
+
+    def run(self):
+        text = f.read()
+        self.mySignal1.emit(text)
 
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -31,33 +29,36 @@ class mywindow(QtWidgets.QMainWindow):
 
         self.outputObject = Output()
         self.outputObject.start()
-        # print(text)
-
         self.outputObject.mySignal1.connect(self.onOutput1, Qt.QueuedConnection)
-        # self.outputObject.mySignal2.connect(self.onOutput2, Qt.QueuedConnection)
-
-    def onOutput1(self):
-        self.ui.output.clear()
-        print(text)
         
-
-    # def onOutput2(self, text):
-        # self.ui.output.setText(text)
-
+    def onOutput1(self, text):
+        self.ui.output.clear()
+        self.ui.output.setText(text)
+        
     def keyPressEvent(self, e):
+        global f
         if e.key() == Qt.Key_Escape:
             f.close()
             self.close()
  
-app = QtWidgets.QApplication([])
-
-# разрешение экрана:
-q= QDesktopWidget().availableGeometry()
+# --------------------------------------------------------
 
 if (len(sys.argv) != 2):
     print("Usage: Chat <name>")
     sys.exit()
 name = sys.argv[1]
+
+if (os.path.exists("log.txt")):
+    f = open("log.txt")
+else: 
+    f = open("log.txt", "w")
+    f.close()
+    f = open("log.txt")
+
+app = QtWidgets.QApplication([])
+
+# разрешение экрана:
+q= QDesktopWidget().availableGeometry()
 
 
 application = mywindow()
