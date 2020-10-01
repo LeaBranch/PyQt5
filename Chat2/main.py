e46a4b4a4dcada9bc20e3cb69e711210ea693e76
elizaveta <rubanskaya.elizaveta@mail.ru>
2020-10-01 13:16:20 +0700

    add input thread

--- Chat2/main.py
+++ Chat2/main.py
@@ -3,10 +3,12 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QApplication, QDesktopWidget
 
 from gui import Ui_MainWindow  # импорт нашего сгенерированного файла
-import sys, os
+import sys, os, time
  
 
 name = ''
+# outputText = ''
+input_text = ''
 
 class Output(QtCore.QThread):
     global f
@@ -16,8 +18,24 @@ class Output(QtCore.QThread):
         QtCore.QThread.__init__(self, parent)
 
     def run(self):
-        text = f.read()
-        self.mySignal1.emit(text)
+        global outputText
+        while (1):
+            outputText = f.read()
+            self.mySignal1.emit(outputText)
+            f.seek(0)
+            # time.sleep(1)
+
+
+class Input(QtCore.QThread):
+    def __init__(self, parent=None):
+        QtCore.QThread.__init__(self, parent)
+
+    def run(self):
+        global f
+        global input_text
+        print("this is message:", input_text)
+        f.seek(2)
+        f.write(name + ": " + input_text + "\n")
 
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -25,35 +43,48 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+# ------вывод имени--------------------
         self.ui.label.setText(name)
+# ------включение кнопки---------------
+        self.ui.sendButton.clicked.connect(self.inputThreadCall)
 
+# ------вызов потока OUTPUT------------
         self.outputObject = Output()
         self.outputObject.start()
-        self.outputObject.mySignal1.connect(self.onOutput1, Qt.QueuedConnection)
-        
-    def onOutput1(self, text):
-        self.ui.output.clear()
+
+        self.outputObject.mySignal1.connect(self.onOutput, Qt.QueuedConnection)
+
+# ------исполнение GUI для OUTPUT-------------
+    def onOutput(self, text):
+        # global outputText
+        # self.ui.output.clear()
         self.ui.output.setText(text)
+
+# ------вызов потока INPUT-------------
+    def inputThreadCall(self):
+        global input_text
+        input_text = self.ui.input.toPlainText()
+        
+        self.inputObject = Input()
+        self.inputObject.start()
+
+        self.ui.input.clear()
         
     def keyPressEvent(self, e):
         global f
         if e.key() == Qt.Key_Escape:
             f.close()
             self.close()
+
  
-# --------------------------------------------------------
+#=======================================================
 
 if (len(sys.argv) != 2):
     print("Usage: Chat <name>")
     sys.exit()
 name = sys.argv[1]
 
-if (os.path.exists("log.txt")):
-    f = open("log.txt")
-else: 
-    f = open("log.txt", "w")
-    f.close()
-    f = open("log.txt")
+f = open("log.txt", "r+")
 
 app = QtWidgets.QApplication([])
 
